ae3ccdaa7a1cdf59b7d9b5426a141c50a36bd037
David Piesse <piesse@gmail.com>
2020-04-16 20:39:54 +0100

    wip

--- resources/js/components/fields/DetailField.vue
+++ resources/js/components/fields/DetailField.vue
@@ -1,6 +1,6 @@
 <template>
   <panel-item :field="field">
-    <gallery slot="value" :value="orderedValue" :field="field" :multiple="field.multiple" />
+    <gallery slot="value" :value="field.value" :field="field" :multiple="field.multiple" />
   </panel-item>
 </template>
 
@@ -12,29 +12,27 @@ export default {
     Gallery
   },
   computed: {
-    orderedValue() {
-      console.log(this.field.value);
-      // Todo get this to sort by the group where possible
-      return this.field.value.sort((a, b) => {
-        if (a.custom_properties.group && b.custom_properties.group) {
-          if (a.custom_properties.group < b.custom_properties.group) {
-            return -1;
-          }
-          if (a.custom_properties.group > b.custom_properties.group) {
-            return 1;
-          }
-          return 0;
-        } else {
-          if (a.file_name < b.file_name) {
-            return -1;
-          }
-          if (a.file_name > b.file_name) {
-            return 1;
-          }
-          return 0;
-        }
-      });
-    }
+    // orderedValue() {
+    //   return this.field.value.sort((a, b) => {
+    //     if (a.custom_properties.group && b.custom_properties.group) {
+    //       if (a.custom_properties.group < b.custom_properties.group) {
+    //         return -1;
+    //       }
+    //       if (a.custom_properties.group > b.custom_properties.group) {
+    //         return 1;
+    //       }
+    //       return 0;
+    //     } else {
+    //       if (a.created_at < b.created_at) {
+    //         return -1;
+    //       }
+    //       if (a.created_at > b.created_at) {
+    //         return 1;
+    //       }
+    //       return 0;
+    //     }
+    //   });
+    // }
   },
   props: ["resource", "resourceName", "resourceId", "field"]
 };
