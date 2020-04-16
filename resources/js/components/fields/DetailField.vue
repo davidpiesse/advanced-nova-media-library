dc9276a6ce91805a10d78ca286746780bc0acc03
David Piesse <piesse@gmail.com>
2020-04-16 14:40:12 +0100

    wip

--- resources/js/components/fields/DetailField.vue
+++ resources/js/components/fields/DetailField.vue
@@ -14,6 +14,7 @@ export default {
   computed: {
     orderedValue() {
       console.log(this.field.value);
+      // Todo get this to sort by the group where possible
       return this.field.value.sort((a, b) => {
         if (a.custom_properties.group && b.custom_properties.group) {
           if (a.custom_properties.group < b.custom_properties.group) {
@@ -23,8 +24,15 @@ export default {
             return 1;
           }
           return 0;
+        } else {
+          if (a.file_name < b.file_name) {
+            return -1;
+          }
+          if (a.file_name > b.file_name) {
+            return 1;
+          }
+          return 0;
         }
-        return 0;
       });
     }
   },
