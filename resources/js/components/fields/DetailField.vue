874bdc648397b737a02938636b0709ae3391cc20
David Piesse <piesse@gmail.com>
2020-04-16 20:44:04 +0100

    wip

--- resources/js/components/fields/DetailField.vue
+++ resources/js/components/fields/DetailField.vue
@@ -11,29 +11,6 @@ export default {
   components: {
     Gallery
   },
-  computed: {
-    // orderedValue() {
-    //   return this.field.value.sort((a, b) => {
-    //     if (a.custom_properties.group && b.custom_properties.group) {
-    //       if (a.custom_properties.group < b.custom_properties.group) {
-    //         return -1;
-    //       }
-    //       if (a.custom_properties.group > b.custom_properties.group) {
-    //         return 1;
-    //       }
-    //       return 0;
-    //     } else {
-    //       if (a.created_at < b.created_at) {
-    //         return -1;
-    //       }
-    //       if (a.created_at > b.created_at) {
-    //         return 1;
-    //       }
-    //       return 0;
-    //     }
-    //   });
-    // }
-  },
   props: ["resource", "resourceName", "resourceId", "field"]
 };
 </script>
